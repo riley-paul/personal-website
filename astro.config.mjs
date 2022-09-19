735a3f7240038b6a3fb4af0708194333f4b51061
Riley Paul <rileypaul96@gmail.com>
2022-09-19 14:21:15 -0700

    Create react responsive navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  integrations: [tailwind(), alpinejs(), react()],
   site: "https://rileypaul.ca"
 });
