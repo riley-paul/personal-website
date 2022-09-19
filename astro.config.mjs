bd404894159a502aab2f6c6f5f527a6ff46338d9
Riley Paul <rileypaul96@gmail.com>
2022-09-19 13:59:32 -0700

    get responsive header working with alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site:"https://rileypaul.ca"
+  integrations: [tailwind(), alpinejs()],
+  site: "https://rileypaul.ca"
 });
