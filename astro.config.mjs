e4dfbbb8c507f5099942b325208fb12933e82f52
Riley Paul <rileypaul96@gmail.com>
2022-09-20 16:46:50 -0700

    create simple navbar using alpine and remove react from project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-import react from "@astrojs/react";
-
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), react(), mdx()],
-  site: "https://rileypaul.ca"
+  integrations: [tailwind(), alpinejs()],
+  site: "https://rileypaul.ca",
 });
