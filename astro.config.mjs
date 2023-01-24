7de3413fa33f9c6a5ff514a009f6e3ffb0a62ea9
Riley Paul <rileypaul96@gmail.com>
2023-01-24 08:48:38 -0800

    remove mdx and alpine integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), mdx()],
+  integrations: [tailwind()],
   site: "https://rileypaul.ca"
 });
