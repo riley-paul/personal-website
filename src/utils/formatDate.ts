363404300a3e21f462fd93abd9796c053a2265de
Riley Paul <rileypaul96@gmail.com>
2023-05-05 13:00:17 -0700

    sort experience by dates

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,4 @@
-export default function formatDate(date) {
+export default function formatDate(date: string): string {
   return new Date(date).toLocaleDateString("en-GB", {
     timeZone: "UTC",
     month: "short",
