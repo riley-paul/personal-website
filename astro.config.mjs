6bb492e4af6be13d73fb9030f8169d03452483a9
Riley Paul <rileypaul96@gmail.com>
2023-07-24 12:55:37 -0700

    add shadcn componet library
    update linklist
    
    update project card with shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
-  site: "https://rileypaul.ca"
+  integrations: [tailwind({ applyBaseStyles: false }), alpinejs(), react()],
+  site: "https://rileypaul.ca",
 });
