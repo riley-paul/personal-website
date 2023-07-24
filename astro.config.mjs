5ed3c473ee3fe120da44ecec087a52f0d2980961
Riley Paul <rileypaul96@gmail.com>
2023-07-24 12:56:26 -0700

    Revert "add shadcn componet library"
    This reverts commit 6bb492e4af6be13d73fb9030f8169d03452483a9.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), alpinejs(), react()],
-  site: "https://rileypaul.ca",
+  integrations: [tailwind(), alpinejs()],
+  site: "https://rileypaul.ca"
 });
